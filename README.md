ddbfdf4c1d95b65e7e621ebc5f6d0d3185e9a3e8
Shay Green <shagreen@pdx.edu>
2021-05-16 11:06:44 -0700

    Adding starter files

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-RECIPE BOX
+### RECIPE BOX
+
+### Pages
+
+1. Home Page
+
+   - 3 links to other three pages (navbar)
+   - Express routing
+
+2. Add your own recipe
+3. Find recipe from url
+4. Search recipes from API
+
+### APIS
+
+### Modules and Libraries
+
+Express
+Bootstrap
---
+++ index.js
@@ -0,0 +1,59 @@
+//Express routing here to the three pages
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+const routes = [
+    'Search',
+    'AddNew',
+    'Browse',
+  ];
+
+  let getRoutes = () => {
+    let result = '';
+  
+    routes.forEach(
+      (elem) => (result += `<li><a href="/${elem}">${elem}</a></li>`)
+    );
+  
+    return result;
+  };
+  
+  app.get('/', (req, res) => {
+    let routeResults = getRoutes();
+  
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(`<h1>Recipe Book</h1>`);
+    res.write(`<ul> ${routeResults} </ul>`);
+    res.end();
+  });
+  
+  // Add your code here
+  app.get('/Search', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(`<h1>Welcome!</h1>`);
+    res.end();
+  });
+  
+  app.get('/AddNew', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write("<h1>Welcome!</h1>");
+    res.end();
+  });
+  
+  app.get('/Browse', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write("<h1>Welcome!</h1>");
+    res.end();
+  });
+
+  app.get('*', (req, res) => {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.write("<h1>404: Page not found</h1>");
+    res.end();
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+  
