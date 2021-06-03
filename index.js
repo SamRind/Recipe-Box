5714cb8d4e969a0479b192b68e5d5d6dc5068cea
Sam Rind <orind@pdx.edu>
2021-06-03 15:08:38 -0700

    working with auth

--- index.js
+++ index.js
@@ -11,6 +11,227 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local')
 const http = require('http');
 
+//Starting database portion here 
+require("dotenv").config()
+const mongoose = require("mongoose")
+mongoose.connect("mongodb+srv://orind:database21!@cluster0.mc5cu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true 
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Error: " + err.message)
+})
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connected successfully")
+})
+
+// //Passoword authentications
+  app.use(session({
+  secret:"our_little secret",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/api/users', require('./routes/user'));
+
+app.use(
+  parser.urlencoded({
+    extended: false,
+    limit: 1024,
+  })
+);
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+
+app.get('/', (req, res) => {
+    res.render('home', {});
+});
+
+app.get('/About', (req, res) => {
+    res.render('about', {})
+});
+
+app.get('/search', (req, res) => {
+    res.render('search', {recipe:[]});
+    //res.sendFile(path.join(__dirname + '/public/search.html'));
+
+});
+  
+app.post('/search', (req, res) => {
+    //res.writeHead(200, { 'Content-Type': 'text/html' });
+    var lookup = req.body.search;
+    var searchurl = {
+    method: 'GET',
+    url: 'https://edamam-recipe-search.p.rapidapi.com/search',
+    params: {q: lookup},
+    headers: {
+      'x-rapidapi-key': 'f1ca9f8871msh23516a13e3aa006p1daa57jsn9836fc2f2102',
+      'x-rapidapi-host': 'edamam-recipe-search.p.rapidapi.com'
+      }
+    };
+
+    axios.request(searchurl).then(function (response) {
+	  console.log(response.data);
+    console.log(response.data.hits[0]);
+    console.log(response.data.hits[0].recipe.label)
+    let recipelist = [];
+    response.data.hits.forEach(element =>{
+      //so here create object to put stuff into build object and push into list
+      //ingred.push(ingredientLines);
+      var tofill = {name: element.recipe.label, image: element.recipe.image, rUrl: element.recipe.url, yeild: element.recipe.yield, ingredients: element.recipe.ingredientLines};
+      recipelist.push(tofill);
+      //recipelist.push(element.recipe.label)
+    })
+    res.render('search', {recipe: recipelist});
+    }).catch(function (error) {
+	      console.error(error);
+        res.render('search', {recipe:[]});
+      });
+    //res.end();
+  });
+
+app.get('/AddNew', (req, res) => {
+    res.render('add', {});
+});
+
+const recipe = require("./model/recipe.js")
+
+app.post('/submit', (req, res) => {
+const Add = new recipe({
+                    _id: new mongoose.Types.ObjectId(),
+                    name: req.body.name,
+                    Ingredients: req.body.Ingredients, 
+                    Prep: req.body.Prep,
+                    Bookmark: req.body.Bookmark,
+                })
+                Add.save();
+                res.end(); 
+});
+
+const user = require("./model/model.js");
+app.post('/submituser', (req, res) => {
+
+  if (finduser(req.params.username) === true)
+  {
+    console.log("user does exist so just login") 
+    res.end(); 
+  } 
+const Add = new user({
+                    _id: new mongoose.Types.ObjectId(),
+                    username: req.body.name,
+                    email: req.body.email, 
+                    password: req.body.password,
+                    role: req.body.role,
+                })
+                console.log("this is else block test")
+                Add.save();
+                res.end(); 
+});
+
+  async finduser = (username, next) => { 
+  user.findOne({username: username}, function(err, user){
+        if(err) {
+          console.log(err);
+        }
+        var message;
+        console.log("User", user); 
+        if(user) {
+            console.log(user)
+            message = "user exists";
+            console.log(message)
+            return true; 
+        } else {
+            message= "user doesn't exist";
+            console.log(message)
+            return false; 
+        }
+        return message; 
+       // res.json({message: message});
+    });
+  }
+
+app.get('/Browse', (req, res) => {
+  res.render('browse', {});
+  });
+  app.post('/Browse', (req, res)=> {
+    const url = "mycookbook-io1.p.rapidapi.com"
+    const apiKey = "d2b5a72c7amshb4dcaa17f424b8fp18e512jsn75615a6fc853"
+    //res.writeHead(200, { 'Content-Type': 'text/html' });
+    var recipe = req.body.Url;
+    var browseReq = {
+        method: 'POST',
+        url: 'https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi',
+        headers: {
+            'content-type': 'text/plain',
+            'x-rapidapi-key': apiKey,
+            'x-rapidapi-host': url
+        },
+        data: recipe
+    };
+
+    axios.request(browseReq).then(function (response) {
+      let im = "/public/images/default-image.jpg";
+      if (response.data[0].images.length >= 1){
+        im = response.data[0].images[0];
+    
+      res.render('recipe', {
+        recipe: response.data[0].name,
+        description: response.data[0].description,
+        url: recipe,
+        yields: response.data[0].yield,
+        image: im,
+        ingredients: response.data[0].ingredients,
+        //instructions: instructions,
+        instructions: response.data[0].instructions[0].steps,
+      });
+
+    }).catch(function (error) {
+	    console.error(error);
+      res.render('error', {message: "We couldn't parse the specified URL, please try another url."});
+    });
+});
+
+app.get("/MyBox", (req, res) => {
+    res.render('mybox', {})
+});
+
+app.get("/LogIn", (req, res) => {
+    res.render('login', {})
+});
+
+app.post("/LogIn", (req, res) => {
+    res.render('mybox',{})
+});
+
+app.get("/SignUp", (req, res) => {
+      res.render('signup', {})
+  });
+
+  app.post("/SignUp", (req, res) => {
+      res.render('mybox', {})
+  });
+
+
+
+  app.get('*', (req, res) => {
+    res.render('error', {message: "404: Page not found"});
+  });
+  
+  const server = http.createServer(app);
+    server.listen(port, () => console.log(`Server running at http://localhost:${port}`)); 
+
+  
+
+ 
+ 
 //const router = require('express'); 
 
 // app.use(passport.initialize());
@@ -36,15 +257,6 @@ const http = require('http');
 //...
 //app.use(express.static(path.join(__dirname, 'public')));
 
-//Starting database portion here 
-require("dotenv").config()
-const mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://orind:database21!@cluster0.mc5cu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true 
-});
-
 //require("./test/sample")
 //require("./test/users")
 
@@ -58,15 +270,6 @@ mongoose.connect("mongodb+srv://orind:database21!@cluster0.mc5cu.mongodb.net/myF
 //const users = require('./test/users')
 //const sample = require('/test/sample');
 //const  { connect } = require('http2')
-
-mongoose.connection.on("error", (err) => {
-  console.log("Error: " + err.message)
-})
-
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connected successfully")
-})
-
 
 
 //const sample = mongoose.model("sample")
@@ -108,31 +311,6 @@ mongoose.connection.once("open", () => {
 // //app.use('/login', auth);
 // app.use('/login', require('./routes/users')); 
 // //Passoword authentications
-  app.use(session({
-  secret:"our_little secret",
-  resave: false,
-  saveUninitialized: false
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use('/api/users', require('./routes/user'));
-
-app.use(
-  parser.urlencoded({
-    extended: false,
-    limit: 1024,
-  })
-);
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', __dirname + '/views');
-app.set('view engine', 'pug');
-
-
-  app.get('/', (req, res) => {
-    res.render('home', {});
-  });
 
   // //Registration for user
   // app.post('/register', (req, res) => {
@@ -149,208 +327,37 @@ app.set('view engine', 'pug');
   //   })
   // })
   //Reg)istration for user 
-  
-  
-  app.get('/About', (req, res) => {
-    res.render('about', {})
-  });
 
-  app.get('/search', (req, res) => {
-    res.render('search', {recipe:[]});
-    //res.sendFile(path.join(__dirname + '/public/search.html'));
-
-  });
-  app.post('/search', (req, res) => {
-    //res.writeHead(200, { 'Content-Type': 'text/html' });
-    var lookup = req.body.search;
-    var searchurl = {
-    method: 'GET',
-    url: 'https://edamam-recipe-search.p.rapidapi.com/search',
-    params: {q: lookup},
-    headers: {
-      'x-rapidapi-key': 'f1ca9f8871msh23516a13e3aa006p1daa57jsn9836fc2f2102',
-      'x-rapidapi-host': 'edamam-recipe-search.p.rapidapi.com'
-      }
-    };
-
-    axios.request(searchurl).then(function (response) {
-	  console.log(response.data);
-    console.log(response.data.hits[0]);
-    console.log(response.data.hits[0].recipe.label)
-    //res.write(`<h1>${response.data.hits[0].recipe.label.toString()}</h1>`);
-    //res.render('search', {results=recipelist});
-    //res.write(`<img src=${response.data.hits[0].recipe.image}>` );
-    let recipelist = [];
-    //let ingred = [];
-    //so here create object to put stuff into
-    //forEach
-    //res.end();
-    response.data.hits.forEach(element =>{
-      //so here create object to put stuff into build object and push into list
-      //ingred.push(ingredientLines);
-      var tofill = {name: element.recipe.label, image: element.recipe.image, rUrl: element.recipe.url, yeild: element.recipe.yield, ingredients: element.recipe.ingredientLines};
-      recipelist.push(tofill);
-
-    
-      
-      
-
-      //recipelist.push(element.recipe.label)
-    })
-    res.render('search', {recipe: recipelist});
-    }).catch(function (error) {
-	      console.error(error);
-        res.render('search', {recipe:[]});
-        //res.end();
-      });
-    //res.end();
-  });
-  
-  // app.use('/AddNew', express.static(path.join(__dirname, '/react/build')));
+    // app.use('/AddNew', express.static(path.join(__dirname, '/react/build')));
   // app.get('/AddNew/*', (req, res) => {
   //   res.sendFile(path.join(__dirname + '/react/build/index.html'));
   // });
 
-  app.get('/AddNew', (req, res) => {
-    res.render('add', {});
-  });
-
-  const recipe = require("./model/recipe.js")
-
-  app.post('/submit', (req, res) => {
   // res.write(`<p>Name: ${req.body.name}</p>`);
   // res.write(`<p>Ingredient: ${req.body.Ingredients}</p>`);
   // res.write(`<p>Prep: ${req.body.Prep}</p>`);
   // let Bookmark = (req.body.signup) ? 'Bookmark my recipe' : 'No, thank you.';
   // res.write(`<p> : ${Bookmark}</p>`);
   // res.end();
-  const Add = new recipe({
-                    _id: new mongoose.Types.ObjectId(),
-                    name: req.body.name,
-                    Ingredients: req.body.Ingredients, 
-                    Prep: req.body.Prep,
-                    Bookmark: req.body.Bookmark,
-                })
-                Add.save();
-                res.end(); 
-});
 
-  const user = require("./model/model.js")
-  app.post('/submituser', (req, res) => {
- const Add = new user({
-                    _id: new mongoose.Types.ObjectId(),
-                    username: req.body.name,
-                    email: req.body.email, 
-                    password: req.body.password,
-                    role: req.body.role,
-                })
-                Add.save();
-                res.end(); 
-});
+  // app.get('/usercheck', function(req, res) {
+//     User.findOne({username: req.query.username}, function(err, user){
+//         if(err) {
+//           console.log(err);
+//         }
+//         var message;
+//         if(user) {
+//           console.log(user)
+//             message = "user exists";
+//             console.log(message)
+//         } else {
+//             message= "user doesn't exist";
+//             console.log(message)
+//         }
+//         res.json({message: message});
+//     });
+// });
 
+// router.post('/login', function(req, res, next){
 
-  app.get('/Browse', (req, res) => {
-    res.render('browse', {});
-    //res.sendFile(path.join(__dirname + '/public/browse.html'));
-  });
-  app.post('/Browse', (req, res)=> {
-    const url = "mycookbook-io1.p.rapidapi.com"
-    const apiKey = "d2b5a72c7amshb4dcaa17f424b8fp18e512jsn75615a6fc853"
-    //res.writeHead(200, { 'Content-Type': 'text/html' });
-    var recipe = req.body.Url;
-    var browseReq = {
-        method: 'POST',
-        url: 'https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi',
-        headers: {
-            'content-type': 'text/plain',
-            'x-rapidapi-key': apiKey,
-            'x-rapidapi-host': url
-        },
-        data: recipe
-    };
-
-    axios.request(browseReq).then(function (response) {
-	    // console.log(response.data[0]);
-      // res.write("<h1>Good Request</h1>");
-      // res.write(`<h1>${response.data[0].name.toString()}</h1>`);
-      // res.write(`<h2>Yields: ${response.data[0].yield.toString()}</h2>`);
-      // res.write(`<p>${response.data[0].description.toString()}</p>`);
-      // res.write("<h3>Ingredients</h3>");
-      // res.write("<ul>");
-      // response.data[0].ingredients.forEach(element => {
-      //   res.write(`<li>${element.toString()}</li>`);
-      // });
-      // res.write("</ul>");
-      // res.write("<h3>Instructions</h3>");
-      // res.write("<ul>");
-      // response.data[0].instructions[0].steps.forEach(element => {
-      //   res.write(`<li>${element.toString()}</li>`);
-      // });
-      // res.write("</ul>");
-
-      // console.log(response.data[0].instructions)
-
-      // res.end();
-      let im = "/public/images/default-image.jpg";
-      if (response.data[0].images.length >= 1){
-        im = response.data[0].images[0];
-      }
-      // console.log(im)
-      // let instructions = []
-      // response.data[0].instructions[0].steps.forEach(element => {
-      //   console.log(element)
-      //   console.log(element.toString())
-      //   instructions.push(element.toString())
-      // });
-      res.render('recipe', {
-        recipe: response.data[0].name,
-        description: response.data[0].description,
-        url: recipe,
-        yields: response.data[0].yield,
-        image: im,
-        ingredients: response.data[0].ingredients,
-        //instructions: instructions,
-        instructions: response.data[0].instructions[0].steps,
-      });
-
-    }).catch(function (error) {
-	    console.error(error);
-      // res.write("<h1>Sorry, we cannot load this recipe</h1>")
-      // res.end();
-      res.render('error', {message: "We couldn't parse the specified URL, please try another url."});
-    });
-});
-
-  app.get("/MyBox", (req, res) => {
-    res.render('mybox', {})
-  
-  });
-
-  app.get("/LogIn", (req, res) => {
-    res.render('login', {})
-  });
-
-  app.post("/LogIn", (req, res) => {
-    res.render('mybox',{})
-  });
-
-  
-
-  app.get("/SignUp", (req, res) => {
-      res.render('signup', {})
-  });
-
-  app.get('*', (req, res) => {
-    // res.writeHead(404, { 'Content-Type': 'text/html' });
-    // res.write("<h1>404: Page not found</h1>");
-    // res.end();
-    res.render('error', {message: "404: Page not found"});
-
-  });
-  
-  const server = http.createServer(app);
-    server.listen(port, () => console.log(`Server running at http://localhost:${port}`)); 
-
-  
-
-  
+// });
--- routes/user.js
+++ routes/user.js
@@ -62,4 +62,31 @@ router.get('/logout', (req, res) => {
     else return res.json(false);
 });
 
+// router.post('/register', function(req, res, next){
+//     const saltHash = utils.genPassword(req.body.password);
+    
+//     const salt = saltHash.salt;
+//     const hash = saltHash.hash;
+
+//     const newUser = new User({
+//         username: req.body.username,
+//         hash: hash,
+//         salt: salt
+//     });
+
+//     try {
+    
+//         newUser.save()
+//             .then((user) => {
+//                 res.json({ success: true, user: user });
+//             });
+
+//     } catch (err) {
+        
+//         res.json({ success: false, msg: err });
+    
+//     }
+
+// });
+
 module.exports = router;
